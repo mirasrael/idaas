4511ea5b7e3271e506b41d72e8c72824440ac0bb
Mirasrael <alexander.i.bondarev@gmail.com>
2013-03-07 23:23:08 +0400

    Move generated thrift cpp code to separate library

--- generate.cmd
+++ generate.cmd
@@ -1,5 +1,5 @@
 %THIRD_PARTY%\thrift\thrift-0.9.0.exe --gen cpp idaas.thrift
 del gen-cpp\*.skeleton.cpp /Q
-copy gen-cpp\*.cpp idaas
-copy gen-cpp\*.h idaas
+copy gen-cpp\*.cpp libidaas.thrift
+copy gen-cpp\*.h libidaas.thrift
 rmdir gen-cpp /Q /S
